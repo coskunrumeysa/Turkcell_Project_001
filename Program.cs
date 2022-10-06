2e0a82b214583b86d9c88ae40b5b7f0bd880776b
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-10-06 16:05:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Rumeysa Coksun - Computer Engineer
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turkcell_Project_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** KİMLİK *****");
+            Console.WriteLine();
+            Console.WriteLine("Rümeysa");
+            Console.WriteLine("Coşkun");
+            Console.WriteLine("Üsküdar Üniversitesi");
+            Console.WriteLine("İstanbul");
+            Console.WriteLine();
+            Console.Write("***** KİMLİK *****");
+            Console.Read();
+           
+        }
+    }
+}
